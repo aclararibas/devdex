a7112c4d576e717c48cb8da64f4633cd3df56297
Ricardo Gomes <aclararibas@gmail.com>
2021-10-18 13:10:33 +0100

    init

---
+++ src/app/core/services/follow.service.ts
@@ -0,0 +1,49 @@
+import { GithubService } from './github.service';
+import { Follow } from './../../shared/models/follow';
+import { Injectable } from '@angular/core';
+import { UserService } from './user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FollowService {
+
+  followPerPage = 8;
+  listFollowers: Follow[] = [];
+  listFollowing: Follow[] = [];
+  loading: boolean = false;
+  pageFollowers = 0;
+  pageFollowing = 0;
+
+  constructor(private userService: UserService, private githubService: GithubService) { }
+
+  getFollowersPerPage() {
+    let page = Math.ceil(this.listFollowers.length / this.followPerPage) + 1;
+
+    if(this.pageFollowers != page) {
+      this.pageFollowers = page;
+    } else { return; }
+
+    this.loading = true;
+    this.githubService.getFollowersPerPage(this.userService.user.login, this.pageFollowers, this.followPerPage)
+      .subscribe((follow) => {
+        this.listFollowers = this.listFollowers.concat(follow);
+        this.loading = false;
+      });
+  }
+
+  getFollowingPerPage() {
+    let page = Math.ceil(this.listFollowing.length / this.followPerPage) + 1;
+
+    if(this.pageFollowing != page) {
+      this.pageFollowing = page;
+    } else { return; }
+
+    this.loading = true;
+    this.githubService.getFollowingPerPage(this.userService.user.login, this.pageFollowing, this.followPerPage)
+      .subscribe((follow) => {
+        this.listFollowing = this.listFollowing.concat(follow);
+        this.loading = false;
+      });
+  }
+}
---
+++ src/app/core/services/login.service.ts
@@ -0,0 +1,56 @@
+import { GithubService } from './github.service';
+import { StarredService } from './starred.service';
+import { UserService } from 'src/app/core/services/user.service';
+import { Injectable } from '@angular/core';
+import { NavigateService } from './navigate.service';
+import { environment } from 'src/environments/environment';
+import { user } from 'src/app/shared/models/user';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import { TranslatorService } from './translator.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginService {
+  constructor(
+    public userService: UserService,
+    public starredService: StarredService,
+    private githubService: GithubService,
+    private navigateService: NavigateService,
+    private _snackBar: MatSnackBar,
+    private translator: TranslatorService,
+  ) {}
+
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+
+  login(login: string) {
+    this.userService.loading = true;
+    this.githubService.getUser(login).subscribe((data: user) => {
+      this.userService.loading = false;
+      this.userService.user = data;
+      if (!this.userService.existUser()) {
+        this.navigateService.navigateToHomeNoUser();
+      } else {
+        this.userService.linkUser = `${environment.url}user/${this.userService.user.login}`;
+        this.setTitle(`${this.userService.user.name}`);
+        this.starredService.pageTotalStarred = 0;
+        this.starredService.getStarred();
+      }
+    }, (error) => {
+      this.userService.loading = false;
+      this._snackBar.open(this.translator.get('snackbar.userNotFound'), 'X', {
+        duration: 1500,
+        horizontalPosition: this.horizontalPosition,
+        verticalPosition: this.verticalPosition,
+        panelClass: ['custom-class'],
+      });
+      this.navigateService.navigateToHomeNoUser();
+    });
+  }
+
+  setTitle(name: string) {
+    name != 'null' ? (window.document.getElementsByTagName('title')[0].innerHTML = `DevDex | ${name}`)
+      : window.document.getElementsByTagName('title')[0].innerHTML = `DevDex`;
+  }
+}
